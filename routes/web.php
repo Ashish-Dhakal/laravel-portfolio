a5a336674e9d712879744e54e9f1167f67d0cd6d
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-10-28 20:25:47 +0545

    controller route added

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\EducationController;
 use App\Http\Controllers\PortfolioUpdateController;
+use App\Models\PortfolioUpdate;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,8 @@ use App\Http\Controllers\PortfolioUpdateController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about' , function(){
-    return view('about');
-});
+
+Route::get('/about',[AboutController::class,'about'])->name('about');
+Route::get('/education',[EducationController::class,'education'])->name('education');
+Route::get('/portfolio',[PortfolioUpdateController::class, 'portfolio'])->name('portfolio');
+Route::get('/service',[ServiceController::class,'service'])->name('service');
