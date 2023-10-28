89c3e566af92cfd95e9c983c74568782bb65331f
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-10-28 14:27:59 +0545

    each controller link added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\EducationController;
+use App\Http\Controllers\PortfolioUpdateController;
 
 /*
 |--------------------------------------------------------------------------
