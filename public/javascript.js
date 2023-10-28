d0643cf4bba89e99f4ad4e0ea344a2220760a55e
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-10-28 08:30:48 +0545

    my portfolio

---
+++ public/javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.header-section').mouseenter(function () { 
+
+        // $(".section-header span").css({
+        //     color: "rgb(10, 88, 202)",
+        //     //  border-bottom: "1px solid  rgb(10, 88, 202)",
+        //     //     border-width: "2px",
+        //     background: "black",
+        //  });
+
+    })
+
+    
+})
