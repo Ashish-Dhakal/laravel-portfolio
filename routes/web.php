82fc48312b044e2b9482cf1ad32aec633f8c869c
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-10-28 23:55:53 +0545

    route updated add added for each controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\EducationController;
-use App\Http\Controllers\PortfolioUpdateController;
-use App\Models\PortfolioUpdate;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PortfolioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +22,16 @@ Route::get('/', function () {
 });
 
 Route::get('/about',[AboutController::class,'about'])->name('about');
+Route::post('about/about_add',[AboutController::class, 'about_add'])->name('about_add');
+
+
 Route::get('/education',[EducationController::class,'education'])->name('education');
-Route::get('/portfolio',[PortfolioUpdateController::class, 'portfolio'])->name('portfolio');
+Route::post('education/education_add',[EducationController::class, 'education_add'])->name('education_add');
+
+
+Route::get('/portfolio',[PortfolioController::class, 'portfolio'])->name('portfolio');
+Route::post('portfolio/portfolio_add',[PortfolioController::class, 'portfolio_add'])->name('portfolio_add');
+
+// route related to service
 Route::get('/service',[ServiceController::class,'service'])->name('service');
+Route::post('service/service_add',[ServiceController::class, 'service_add'])->name('service_add');
