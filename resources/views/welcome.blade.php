50b1eb9cfea1ffe9b3a8ee87a4c91bebad2c46fe
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-11-03 12:58:18 +0545

    bootstrap navbar edited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,36 +8,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ashish Dhakal</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous"
-        referrerpolicy="no-referrer" />
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link src="/bootstrap/css/bootstrap.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
     {{-- ------------navbar header starts------------------- --}}
-    <div class="nav-header container">
+
+
+
+    {{-- <div class="nav-header container">
         <div class="hero-name">
             <a href="#"> Ashish Dhakal</a>
         </div>
         <div class="nav-bar" id="Topnav-bar">
             <ul>
-                <li><a href="#">Home</a></li>
                 <li><a href="#About">About</a></li>
                 <li><a href="#Education">Education</a></li>
                 <li><a href="#Education">Experience</a></li>
                 <li><a href="#Portfolio">Portfolio</a></li>
                 <li><a href="#Service">Service</a></li>
                 <li><a href="#Contact">Contact</a></li>
-                <li><div id="menu-btn" class="fas fa-bars"></div></li>
+                <li>
+                    <div id="menu-btn" class="fas fa-bars"></div>
+                </li>
             </ul>
         </div>
+    </div> --}}
+
+    <nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Ashish Dhakal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#About">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#Education">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#Portfolio">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#Service">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#Contact">Contact</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
     {{-- ---------------nav-header ends-------------------- --}}
 
     {{-- -----------------hero section starts------------------ --}}
@@ -59,8 +102,8 @@
 
             <div class="hero-button">
                 <a name="" id="" class="btn btn-primary" href="#Contact" role="button">Contact</a>
-                <a name="button-hero" id="" target=”_blank” class="btn btn-primary" href="https://www.upwork.com/freelancers/~0190ef82bd42e4d819"
-                    role="button">Hire Me</a>
+                <a name="button-hero" id="" target=”_blank” class="btn btn-primary"
+                    href="https://www.upwork.com/freelancers/~0190ef82bd42e4d819" role="button">Hire Me</a>
             </div>
 
         </div>
@@ -85,24 +128,25 @@
 
     <div class="container">
         <section class=" about-section header-section " id="About">
-        <h2 class="section-header "><span>About Me</span></h2>
-        <p class="about-me-parag">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia impedit sed harum
-            eos deleniti porro quos doloremque. Deleniti, dolore ab? Nihil ducimus nulla, quod debitis atque minima
-            voluptate sint praesentium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum velit soluta
-            nemo ainer, veniam tenetur numquam rerum. Repellendus, molestiae at molestias quos pariatur quia ducimus
-            maiores perspiciatis ab dolorem provident?</p>
+            <h2 class="section-header "><span>About Me</span></h2>
+            <p class="about-me-parag">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia impedit sed
+                harum
+                eos deleniti porro quos doloremque. Deleniti, dolore ab? Nihil ducimus nulla, quod debitis atque minima
+                voluptate sint praesentium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum velit soluta
+                nemo ainer, veniam tenetur numquam rerum. Repellendus, molestiae at molestias quos pariatur quia ducimus
+                maiores perspiciatis ab dolorem provident?</p>
 
-        <div class="about-box  ">
-            <div class="box-one">
-                <span class="num">2+</span>
-                <h3>Years <br>experience</h3>
+            <div class="about-box  ">
+                <div class="box-one">
+                    <span class="num">2+</span>
+                    <h3>Years <br>experience</h3>
+                </div>
+                <div class="box-two">
+                    <span class="num">4</span>
+                    <h3>Project <br>Compleated</h3>
+                </div>
             </div>
-            <div class="box-two">
-                <span class="num">4</span>
-                <h3>Project <br>Compleated</h3>
-            </div>
-        </div>
-    </section>
+        </section>
     </div>
 
     {{-- -------------------about section ends ----------------- --}}
@@ -139,68 +183,87 @@
     {{-- ------------------education section ends---------------------- --}}
 
 
-    {{------------------------portofolio section starts------------------}}
+    {{-- ----------------------portofolio section starts---------------- --}}
 
 
-<section class="portfolio container" id="Portfolio">
+    <section class="portfolio container" id="Portfolio">
 
- <h2 class="section-header"><span>Portfolio</span></h2>
+        <h2 class="section-header"><span>Portfolio</span></h2>
 
-<div class="box-container">
+        <div class="box-container">
 
-    <div class="box">
-         <a href="https://github.com/Ashish-Dhakal/Hotel-Management-System" target="_blank"> <img src="img/javaproject.jpg" alt="" ></a> 
-    </div>
-    
-    <div class="box">
-        <a href="https://github.com/Ashish-Dhakal/Ecommerce"  target="_blank" ><img src="img/emall-image.png" alt=""> alt="" ></a> 
-   </div>
+            <div class="box">
+                <a href="https://github.com/Ashish-Dhakal/Hotel-Management-System" target="_blank"> <img
+                        src="img/javaproject.jpg" alt=""></a>
+            </div>
 
-    <div class="box">
-        <a href="https://github.com/Ashish-Dhakal/Ecommerce" target=”_blank”>  <img src="img/travel-web.jpg" alt=""> alt="" ></a>
-    </div>
-   
-    <div class="box">
-        <a href="" target=”_blank”> <img src="img/websiteprj-ing.jpg" alt=""> </a>
-    </div>
+            <div class="box">
+                <a href="https://github.com/Ashish-Dhakal/Ecommerce" target="_blank"><img src="img/emall-image.png"
+                        alt=""> alt="" ></a>
+            </div>
 
-</div>
+            <div class="box">
+                <a href="https://github.com/Ashish-Dhakal/Ecommerce" target=”_blank”> <img src="img/travel-web.jpg"
+                        alt=""> alt="" ></a>
+            </div>
 
-</section>
-    {{------------------------portofolio section ends--------------------}}
+            <div class="box">
+                <a href="" target=”_blank”> <img src="img/websiteprj-ing.jpg" alt=""> </a>
+            </div>
 
-    {{------------------------contact section starts---------------------}}
+        </div>
+
+    </section>
+    {{-- ----------------------portofolio section ends------------------ --}}
+
+    {{-- ----------------------contact section starts------------------- --}}
 
     <section class="service container" id="Service">
         <h2 class="section-header"><span>Service</span></h2>
         <div class="service-box">
-                <div class="service-content">
-                    <h4>web design</h4>
-                    <p><div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum veritatis sapiente, nemo illum ducimus.</div></p>
-                </div>
-                <div class="service-content">
-                    <h4>web design</h4>
-                    <p><div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum veritatis sapiente, nemo illum ducimus.</div></p>
-                </div>
-                <div class="service-content">
-                    <h4>web design</h4>
-                    <p><div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum veritatis sapiente, nemo illum ducimus.</div></p>
-                </div>
-                <div class="service-content">
-                    <h4>web design</h4>
-                    <p><div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum veritatis sapiente, nemo illum ducimus.</div></p>
-                </div>
-                
+            <div class="service-content">
+                <h4>web design</h4>
+                <p>
+                <div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum
+                    corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum
+                    veritatis sapiente, nemo illum ducimus.</div>
+                </p>
             </div>
+            <div class="service-content">
+                <h4>web design</h4>
+                <p>
+                <div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum
+                    corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum
+                    veritatis sapiente, nemo illum ducimus.</div>
+                </p>
+            </div>
+            <div class="service-content">
+                <h4>web design</h4>
+                <p>
+                <div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum
+                    corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum
+                    veritatis sapiente, nemo illum ducimus.</div>
+                </p>
+            </div>
+            <div class="service-content">
+                <h4>web design</h4>
+                <p>
+                <div class="5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo labore cum
+                    corrupti quo asperiores ipsam maxime magnam nulla dicta ipsa dolores enim itaque quae dolorum
+                    veritatis sapiente, nemo illum ducimus.</div>
+                </p>
+            </div>
+
+        </div>
 
     </section>
 
 
-    {{------------------------service section ends-----------------------}}
+    {{-- ----------------------service section ends--------------------- --}}
 
 
 
-    {{------------------------contact section starts---------------------}}
+    {{-- ----------------------contact section starts------------------- --}}
 
     <section class="contact container" id="Contact">
         <h2 class="section-header"><span>Contact</span></h2>
@@ -229,7 +292,8 @@
                 <input class="info" type="text" placeholder="Name"><br>
                 <input class="info" type="text" placeholder="E-mail"><br>
                 <input class="info" type="text" placeholder="Project"><br>
-                <textarea class="info" name="" id="" cols="25" rows="7" placeholder="Your Message......"></textarea><br>
+                <textarea class="info" name="" id="" cols="25" rows="7"
+                    placeholder="Your Message......"></textarea><br>
                 <a href="" class="btn">Submit</a>
             </div>
         </div>
@@ -237,9 +301,9 @@
 
 
 
-    {{------------------------contact section ends-----------------------}}
+    {{-- ----------------------contact section ends--------------------- --}}
 
-  
+
 
 
 
