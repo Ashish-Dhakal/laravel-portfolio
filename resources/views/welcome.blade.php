a577adb9743964cda2dae31cca8e662e8fb9143a
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-11-04 09:49:56 +0545

    bootstrap navbar added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,32 +44,33 @@
     </div> --}}
 
     <nav class="navbar navbar-expand-lg ">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Ashish Dhakal</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#About">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#Education">Education</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#Portfolio">Portfolio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#Service">Service</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#Contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Ashish Dhakal</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#About">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#Education">Education</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#Portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#Service">Service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#Contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
 
 
@@ -257,6 +258,8 @@
         </div>
 
     </section>
+
+ 
 
 
     {{-- ----------------------service section ends--------------------- --}}
