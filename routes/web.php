657dc163d0e1698b2e055c9c593ade9fb39e042d
Rameshacharya22 <acharya232ramesh@gmail.com>
2024-01-18 14:44:35 +0545

    route updated

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about',[AboutController::class,'about'])->name('about');
-Route::post('about/about_add',[AboutController::class, 'about_add'])->name('about_add');
+// Route::get('/about', [AboutController::class, 'about'])->name('about');
+Route::resource('/about', AboutController::class);
+Route::post('about/about_add', [AboutController::class, 'about_add'])->name('about_add');
 
 
-Route::get('/education',[EducationController::class,'education'])->name('education');
-Route::post('education/education_add',[EducationController::class, 'education_add'])->name('education_add');
+Route::get('/education', [EducationController::class, 'education'])->name('education');
+Route::post('education/education_add', [EducationController::class, 'education_add'])->name('education_add');
 
 
-Route::get('/portfolio',[PortfolioController::class, 'portfolio'])->name('portfolio');
-Route::post('portfolio/portfolio_add',[PortfolioController::class, 'portfolio_add'])->name('portfolio_add');
+Route::get('/portfolio', [PortfolioController::class, 'portfolio'])->name('portfolio');
+Route::post('portfolio/portfolio_add', [PortfolioController::class, 'portfolio_add'])->name('portfolio_add');
 
 // route related to service
-Route::get('/service',[ServiceController::class,'service'])->name('service');
-Route::post('service/service_add',[ServiceController::class, 'service_add'])->name('service_add');
+Route::get('/service', [ServiceController::class, 'service'])->name('service');
+Route::post('service/service_add', [ServiceController::class, 'service_add'])->name('service_add');
+
+
+
+Route::get('/showabout', [AboutController::class, 'showAbout'])->name('showAbout');
+
+
+Route::get('/aboutedit/{id}/edit', [AboutController::class, 'aboutEdit'])->name('aboutedit');
+Route::post('/aboutedit/{id}/edit', [AboutController::class, 'abc'])->name('abc');
+
+Route::post('/aboutupdate/{id}/update', [AboutController::class, 'aboutUpdate'])->name('aboutupdate');
