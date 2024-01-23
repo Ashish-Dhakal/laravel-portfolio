cfad2637a5e9c48df569a7a6efa2d42652ddab78
Ashish-Dhakal <ashishdhakal433@gmail.com>
2024-01-23 23:34:41 +0545

    controller added

--- app/Http/Controllers/EducationController.php
+++ app/Http/Controllers/EducationController.php
@@ -3,10 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Education;
 
 class EducationController extends Controller
 {
     public function education(){
         return view('/education');
+    }
+
+    public function education_add(Request $request)
+    {
+
+        //  dd($request->all());
+        //return view('service_add');
+
+        $request->validate([
+            'board' => 'required',
+            'year' => 'required',
+            'description' => 'required'
+        ]);
+
+
+        $service = new Education;
+        $service->board = $request->board;
+        $service->year = $request->year;
+        $service->Description = $request->description;
+
+        $service->save();
+        return  back();
     }
 }
