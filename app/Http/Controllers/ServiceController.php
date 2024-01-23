329eb799205cc462ac40c68ef2d3ed0de57ac177
Rameshacharya22 <acharya232ramesh@gmail.com>
2024-01-23 20:58:49 +0545

    update

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -3,10 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Service;
 
 class ServiceController extends Controller
 {
     public function service(){
         return view('/service');
+    }
+
+    public function service_add( Request $request){
+
+      //  dd($request->all());
+        //return view('service_add');
+
+        $request -> validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+
+        $service = new Service;
+        $service->title = $request->title;
+        $service->Description = $request->description;
+
+        $service->save();
+        return  back();
     }
 }
