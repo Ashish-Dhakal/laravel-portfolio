0e57b7382260a7e9887a98aaad36c60f0830ddf8
Ashish-Dhakal <ashishdhakal433@gmail.com>
2024-01-23 22:22:33 +0545

    route updated

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,16 @@ Route::get('/about',[AboutController::class,'about'])->name('about');
 Route::get('/education',[EducationController::class,'education'])->name('education');
 Route::get('/portfolio',[PortfolioUpdateController::class, 'portfolio'])->name('portfolio');
 Route::get('/service',[ServiceController::class,'service'])->name('service');
+Route::post('about/about_add',[AboutController::class, 'about_add'])->name('about_add');
+
+
+Route::get('/education',[EducationController::class,'education'])->name('education');
+Route::post('education/education_add',[EducationController::class, 'education_add'])->name('education_add');
+
+
+Route::get('/portfolio',[PortfolioController::class, 'portfolio'])->name('portfolio');
+Route::post('portfolio/portfolio_add',[PortfolioController::class, 'portfolio_add'])->name('portfolio_add');
+
+// route related to service
+Route::get('/service',[ServiceController::class,'service'])->name('service');
+Route::post('service/service_add',[ServiceController::class, 'service_add'])->name('service_add');
